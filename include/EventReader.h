eed43fe788f23b2d326a62a192ce89af1d2c4fcd
Caryn Palatchi <cap2wm@virginia.edu>
2021-03-08 08:35:24 -0500

    beam pipe

--- include/EventReader.h
+++ include/EventReader.h
@@ -7,6 +7,7 @@
 #include "globals.hh"
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include <fstream>
+#include "DetectorConstruction.h"
 
 class G4Event;
 class G4GenericMessenger;
@@ -15,7 +16,7 @@ class EventReader : public G4VUserPrimaryGeneratorAction {
 
   public:
 
-    EventReader();
+    EventReader(DetectorConstruction*);
     virtual ~EventReader() {}
 
     virtual void GeneratePrimaries(G4Event *evt);
@@ -25,15 +26,15 @@ class EventReader : public G4VUserPrimaryGeneratorAction {
     void OpenInput(); // function to open COMRAD output
 
     //vertex coordinates and number of tracks
-    void ReadVertex(const std::string& line, G4double& vx, G4double& vy, G4double& vz, int& ntrk);
-
+   void ReadVertex(const std::string& line, G4double& vx, G4double& vy, G4double& vz, 
+		    G4double &uXsec, G4double &pXsec, int& ntrk);
     std::ifstream fIn; //  input file
     G4String fInputName; // name of input file
 
     G4GenericMessenger *fMsg; // messenger for name of input file
     
     int fIev; // event number for progress printout
-
+   DetectorConstruction *fDetConst;
     int fUseBeam;
     double fBeamE,fBeamVX,fBeamVY,fBeamVZ,fBeamPx,fBeamPy,fBeamPz;
 };
